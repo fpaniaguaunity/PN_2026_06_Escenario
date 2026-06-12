6873d6bf07ad13f8027aca7346aa06f2a36ed501
Fernando Paniagua <fernando.paniagua.unity@gmail.com>
2026-06-12 11:57:43 +0200

    Move SWAT assets into EVF folder and update scenes
    Reorganize SWAT 3D assets by moving model, textures, materials, animations and prefab into Assets/EVF (adds EVF folder .meta files). Add Resources performance test JSONs and Scene2, and introduce several new animation/controller/material files and a GestorEscenas.cs script (Amarillo.mat). Update scenes (CafeteriaScene and Demo_Roads) with multiple prefab instance transform/parent adjustments, add GameManager/SceneManager MonoBehaviours and apply lighting changes (UniversalAdditionalLightData added, light intensity/cookie/shape fields updated). Also update EditorBuildSettings and ProjectSettings to reflect these changes. Mostly asset moves and scene prefab/light adjustments.

---
+++ Assets/_GameAssets/scripts/GestorEscenas.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GestorEscenas : MonoBehaviour
+{
+    int totalScenes = 0;
+    int currentSceneIndex = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        totalScenes = SceneManager.sceneCountInBuildSettings;
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (currentSceneIndex < totalScenes - 1)
+            {
+                currentSceneIndex++;
+            } else
+            {
+                currentSceneIndex = 0;
+            }
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+    }
+}
